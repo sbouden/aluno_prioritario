9388c614ac093bafeecbeb4c10f72b0f3960f412
Sonia Bouden <bouden.sonia.s@gmail.com>
2023-04-26 12:39:04 +0200

    jenkinsfile.groovy check docker

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -3,11 +3,21 @@ pipeline {
    stages {
 
       stage('Install Docker') {
-         steps {
-            sh 'curl -fsSL https://get.docker.com -o get-docker.sh'
-            sh 'sudo sh get-docker.sh'
-            sh 'docker --version'
+         
+         // install docker macos
+         stage('Install Docker') {
+            steps {
+               sh '#!/bin/bash\nset -e\n\nif ! command -v docker &> /dev/null\nthen\n    echo "Docker n\'est pas installé. Installation en cours ..."\n    # Installation de Homebrew si nécessaire\n    if ! command -v brew &> /dev/null\n    then\n        echo "Homebrew n\'est pas installé. Installation en cours ..."\n        /bin/bash -c "$(curl -fsSL https://raw.githubusercontent.com/Homebrew/install/HEAD/install.sh)"\n    fi\n    # Installation de Docker\n    brew install docker\n    # Lancement de Docker\n    open /Applications/Docker.app\n    echo "Docker est installé et en cours d\'exécution."\nelse\n    echo "Docker est déjà installé."\nfi'
+               sh 'chmod +x docker_install.sh'
+               sh './docker_install.sh'
+            }
          }
+         // pour un utilisateur linux
+         // steps {
+         //    sh 'curl -fsSL https://get.docker.com -o get-docker.sh'
+         //    sh 'sudo sh get-docker.sh'
+         //    sh 'docker --version'
+         // }
       }
 
       stage('Build and Deploy') {
