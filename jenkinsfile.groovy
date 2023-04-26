d71c7a639fa56943f8ec2de2e9e35a863f026779
Sonia Bouden <bouden.sonia.s@gmail.com>
2023-04-26 12:24:00 +0200

    jenkinsfile.groovy check docker version

--- jenkinsfile.groovy
+++ jenkinsfile.groovy
@@ -1,6 +1,15 @@
 pipeline {
    agent any
    stages {
+
+      stage('Install Docker') {
+         steps {
+            sh 'curl -fsSL https://get.docker.com -o get-docker.sh'
+            sh 'sudo sh get-docker.sh'
+            sh 'docker --version'
+         }
+      }
+
       stage('Build and Deploy') {
          steps {
             // Clone le repository Git
@@ -8,7 +17,6 @@ pipeline {
 
             // Aller dans app1 et construire l'image Docker
             dir('app1') {
-	       sh 'docker --version'
                sh 'docker build -t img-app1 .'
             }
 
